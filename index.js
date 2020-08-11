7cabe167f1fd851667ec9e62b078220f4f58f13d
Yvonne M Pham <yvonnersz@gmail.com>
2020-08-11 11:55:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ class Lunch {
 }
 
 class Dinner {
-  constructor(salad, soup, entree, _dessert) {
+  constructor(salad, soup, entree, dessert) {
     this.salad = salad;
     this.soup = soup;
     this.entree = entree;
-    this._dessert = _dessert;
+    this._dessert = dessert;
   }
 }
